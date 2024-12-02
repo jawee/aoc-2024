566e9288cb2560a1c76e72a6cfc9e3f5fb8e0f33
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-02 16:47:33 +0100

    chaos

--- day2/main.go
+++ day2/main.go
@@ -21,87 +21,98 @@ func main() {
 	}
 	defer file.Close()
 	res := b(file)
-	// res := b(file)
 	fmt.Printf("Res: %d\n", res)
+}
+
+func isSafeB(vals []int) bool {
+	isSafe := true
+	orderDecided := false
+	descending := true
+	for i, val := range vals {
+		if i == 0 {
+			continue
+		}
+
+		if abs(val, vals[i-1]) > 3 || abs(val, vals[i-1]) == 0 {
+			isSafe = false
+			break
+		}
+
+		if !orderDecided {
+			orderDecided = true
+			if val > vals[i-1] {
+				descending = false
+			} else {
+				descending = true
+			}
+		}
+
+		if descending && val > vals[i-1] {
+			isSafe = false
+			break
+		}
+
+		if !descending && val < vals[i-1] {
+			isSafe = false
+			break
+		}
+	}
+	return isSafe
 }
 
 func b(file io.Reader) int {
 	scanner := bufio.NewScanner(file)
 	count := 0
 
-	lineC := 0
 	for scanner.Scan() {
-		isRemoved := false
 		isSafe := true
-		orderDecided := false
-		descending := true
 		line := scanner.Text()
 		split := strings.Split(line, " ")
-		prev := -1
-		for i, v := range split {
+		list := []int{}
+		for _, v := range split {
 			val, _ := strconv.Atoi(v)
-			if prev == -1 {
-				// TODO: need to be able to remove first
-				// next, _ := strconv.Atoi(split[i+1])
-				// if abs(val, next) > 3 || abs(val, next) == 0 {
-				// 	continue
-				// }
-				prev = val
-				continue
-			}
-			if abs(val, prev) > 3 || abs(val, prev) == 0 {
-				if !isRemoved {
-					fmt.Printf("%d removed\n", val)
-					isRemoved = true
-					continue
-				}
-				fmt.Printf("%d %d abs\n", val, prev)
-				isSafe = false
-				// unsafe 
-				break
-			}
-			if !orderDecided {
-				orderDecided = true
-				if val > prev {
-					descending = false
-				} else {
-					descending = true
-				}
-			}
-
-			if descending && val > prev {
-				if !isRemoved {
-					fmt.Printf("%d removed\n", val)
-					isRemoved = true
-					continue
-				}
-				fmt.Printf("%d > %d descending\n", val, prev)
-				isSafe = false
-				break
-			}
-
-			if !descending && val < prev {
-				if !isRemoved {
-					fmt.Printf("%d removed\n", val)
-					isRemoved = true
-					continue
-				}
-				fmt.Printf("%d < %d ascending\n", val, prev)
-				isSafe = false
-				break
-			}
-			prev = val
+			list = append(list, val)
 		}
 
+		fmt.Printf("Handling %+v\n", list)
+		isSafe = isSafeB(list)
+
+		if !isSafe {
+			for i, _ := range list {
+				if i+1 == len(list) {
+					start := deepCopy(list)
+					start = start[:len(start)-1]
+					fmt.Printf("start: %+v %d\n", start, i)
+					isSafe = isSafeB(start)
+					if isSafe {
+						break;
+					}
+					break;
+				}
+				start := deepCopy(list)
+				fmt.Printf("start: %+v %d\n", start, i)
+				newList := append(start[:i], start[i+1:]...)
+				fmt.Printf("%+v: Trying %+v\n", list, newList)
+				isSafe = isSafeB(newList)
+				if isSafe {
+					break;
+				}
+			}
+		}
 		if isSafe {
 			count++
 		}
-
-		fmt.Printf("Line %d\n", lineC)
-		lineC++
 	}
 
 	return count
+}
+
+func deepCopy(a []int) []int {
+	new := []int{}
+	for _, v := range a {
+		new = append(new, v)
+	}
+	return new
 }
 
 func a(file io.Reader) int {
@@ -124,7 +135,7 @@ func a(file io.Reader) int {
 			if abs(val, prev) > 3 || abs(val, prev) == 0 {
 				fmt.Printf("%d %d abs\n", val, prev)
 				isSafe = false
-				// unsafe 
+				// unsafe
 				break
 			}
 			if !orderDecided {
@@ -163,5 +174,3 @@ func abs(a int, b int) int {
 	}
 	return a - b
 }
-
-
