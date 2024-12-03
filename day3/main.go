e74e49cb5407a82e6c49ec7570a096218732d89c
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-03 07:00:33 +0100

    day 3

--- day3/main.go
+++ day3/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	pwd, _ := os.Getwd()
-	// file, err := os.Open(pwd + "/atest.txt")
+	// file, err := os.Open(pwd + "/day3/atest.txt")
 	// file, err := os.Open(pwd + "/day3/btest.txt")
 	file, err := os.Open(pwd + "/day3/input.txt")
 
@@ -20,12 +20,12 @@ func main() {
 		os.Exit(1)
 	}
 	defer file.Close()
-	res := a(file)
+	res := b(file)
 	fmt.Printf("Res: %d\n", res)
 }
 
 type parser struct {
-	line string
+	line []rune
 	pos  int
 }
 
@@ -37,7 +37,109 @@ func (p *parser) nextChar() (rune, bool) {
 	res := p.line[p.pos]
 	p.pos++
 
-	return rune(res), true
+	return res, true
+}
+
+func b(file io.Reader) int {
+	scanner := bufio.NewScanner(file)
+	count := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes := []rune(line)
+		parser := parser{line: runes, pos: 0}
+
+		rune, ok := parser.nextChar()
+		shouldMul := true
+		for ok {
+			if rune == 'm' && shouldMul {
+				n1, n2, isMul := parser.mul()
+				if isMul {
+					count += n1 * n2
+				}
+			}
+			if rune == 'd' {
+				if parser.isDo() {
+					shouldMul = true
+				}
+
+				if parser.isDont() {
+					shouldMul = false
+				}
+			}
+			rune, ok = parser.nextChar()
+		}
+	}
+
+	return count
+}
+
+func (p *parser) isDo() bool {
+	if p.pos+2 >= len(p.line) {
+		return false
+	}
+
+	if p.line[p.pos] == 'o' && p.line[p.pos+1] == '(' && p.line[p.pos+2] == ')' {
+		return true
+	}
+	
+	return false
+}
+
+func (p *parser) isDont() bool {
+	if p.pos+5 >= len(p.line) {
+		return false
+	}
+
+	if p.line[p.pos] == 'o' && p.line[p.pos+1] == 'n' && p.line[p.pos+2] == '\'' && p.line[p.pos+3] == 't' && p.line[p.pos+4] == '(' && p.line[p.pos+5] == ')' {
+		return true
+	}
+	return false
+}
+
+func (p *parser) mul() (int, int, bool) {
+	rune, ok := p.nextChar()
+	if !ok {
+		return 0, 0, false
+	}
+
+	n1 := -1
+	n2 := -1
+
+	if rune == 'u' {
+		rune = p.line[p.pos]
+		if rune == 'l' {
+			rune = p.line[p.pos+1]
+			if rune == '(' {
+				rune = p.line[p.pos+2]
+				number1 := ""
+				c := 2
+				for unicode.IsDigit(rune) {
+					number1 += string(rune)
+					c++
+					rune = p.line[p.pos+c]
+				}
+				if rune == ',' && len(number1) > 0 {
+					c++
+					rune = p.line[p.pos+c]
+					number2 := ""
+					for unicode.IsDigit(rune) {
+						number2 += string(rune)
+						c++
+						rune = p.line[p.pos+c]
+					}
+					if rune == ')' && len(number2) > 0 {
+						n1, _ = strconv.Atoi(number1)
+						n2, _ = strconv.Atoi(number2)
+					}
+				}
+			}
+		}
+	}
+	if n1 != -1 && n2 != -1 {
+		return n1, n2, true
+	}
+	return 0, 0, false
 }
 
 func a(file io.Reader) int {
@@ -46,80 +148,20 @@ func a(file io.Reader) int {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		// split := strings.Split(line, " ")
-		// parser := parser{line: line, pos: 0}
 		runes := []rune(line)
-		for i, v := range runes {
-			rune := v
-			if rune == 'm' {
-				rune = runes[i+1]
-				if rune == 'u' {
-					rune = runes[i+2]
-					if rune == 'l' {
-						rune = runes[i+3]
-						if rune == '(' {
-							rune = runes[i+4]
-							number1 := ""
-							c := 4
-							for unicode.IsDigit(rune) {
-								number1 += string(rune)
-								c++
-								rune = runes[i+c]
-							}
-							if rune == ',' && len(number1) > 0 {
-								c++
-								rune = runes[i+c]
-								number2 := ""
-								for unicode.IsDigit(rune) {
-									number2 += string(rune)
-									c++
-									rune = runes[i+c]
-								}
-								if rune == ')' && len(number2) > 0 {
-									n1, _ := strconv.Atoi(number1)
-									n2, _ := strconv.Atoi(number2)
-									count += n1 * n2
-								}
-							}
-						}
-					}
+		parser := parser{line: runes, pos: 0}
+
+		rune, ok := parser.nextChar()
+		shouldMul := true
+		for ok {
+			if rune == 'm' && shouldMul {
+				n1, n2, isMul := parser.mul()
+				if isMul {
+					count += n1 * n2
 				}
 			}
+			rune, ok = parser.nextChar()
 		}
-		// rune, ok := parser.nextChar()
-		// for ok {
-		// 	if rune == 'm' {
-		// 		rune, ok = parser.nextChar()
-		// 		if rune == 'u' {
-		// 			rune, ok = parser.nextChar()
-		// 			if rune == 'l' {
-		// 				rune, ok = parser.nextChar()
-		// 				if rune == '(' {
-		// 					rune, ok = parser.nextChar()
-		// 					number1 := ""
-		// 					for unicode.IsDigit(rune) {
-		// 						number1 += string(rune)
-		// 						rune, ok = parser.nextChar()
-		// 					}
-		// 					if rune == ',' && len(number1) > 0 {
-		// 						rune, ok = parser.nextChar()
-		// 						number2 := ""
-		// 						for unicode.IsDigit(rune) {
-		// 							number2 += string(rune)
-		// 							rune, ok = parser.nextChar()
-		// 						}
-		// 						if rune == ')' && len(number2) > 0 {
-		// 							n1, _ := strconv.Atoi(number1)
-		// 							n2, _ := strconv.Atoi(number1)
-		// 							count += n1 * n2
-		// 							break
-		// 						}
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
 
 	return count
