90831b118095efd0090ffd3432a85a77e7a0dc52
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-05 08:17:31 +0100

    day5 b chaos

--- day5/main.go
+++ day5/main.go
@@ -20,9 +20,40 @@ func main() {
 		os.Exit(1)
 	}
 	defer file.Close()
-	res := a(file)
-	// res := b(file)
+	// res := a(file)
+	res := b(file)
 	fmt.Printf("%d\n", res)
+}
+
+func b(file io.Reader) int {
+	scanner := bufio.NewScanner(file)
+	sum := 0
+
+	pageRules := make(map[int][]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		split := strings.Split(line, "|")
+		ruleNum, _ := strconv.Atoi(split[0])
+		ruleVal, _ := strconv.Atoi(split[1])
+		pageRules[ruleNum] = append(pageRules[ruleNum], ruleVal)
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		nums := strings.Split(line, ",")
+		pages := []int{}
+		for _, num := range nums {
+			val, _ := strconv.Atoi(num)
+			pages = append(pages, val)
+		}
+
+		sum += getMiddlePages(pages, pageRules)
+	}
+
+	return sum
 }
 
 func a(file io.Reader) int {
@@ -72,7 +103,7 @@ func isCorrectOrder(pages []int, pageRules map[int][]int) bool {
 			}
 
 			if j > i {
-				break;
+				break
 			}
 			for _, rule := range rules {
 				if page == rule {
@@ -85,29 +116,64 @@ func isCorrectOrder(pages []int, pageRules map[int][]int) bool {
 	return isOk
 }
 
-func correctOrder(pages []int, pageRules map[int][]int) int {
-	// isOk := true
-	for i := range pages {
-		if i == 0 {
-			continue
-		}
+func getMiddlePages(pages []int, pageRules map[int][]int) int {
+	if isCorrectOrder(pages, pageRules) {
+		return 0
+	}
 
-		rules := pageRules[pages[i]]
-		for j, page := range pages {
-			if j == i {
+	new := []int{}
+	used := map[int]bool{}
+	for range pages {
+		for _, page := range pages {
+			if _, ok := used[page]; ok {
 				continue
 			}
+			breaksRule := false
+			for key, rules := range pageRules {
+				keyUsed := false
+				for k := range used {
+					if k == key {
+						keyUsed = true
+					}
+				}
+				if keyUsed {
+					continue
+				}
+				ignoreRules := true
+				for _, v := range pages {
+					if key == v {
+						ignoreRules = false
+						break
 
-			if j > i {
-				break;
-			}
-			for _, rule := range rules {
-				if page == rule {
-					// isOk = false
+					}
+				}
+				if ignoreRules {
+					continue
+				}
+				for _, rule := range rules {
+					if rule == page {
+						breaksRule = true
+						break
+					}
+				}
+				if breaksRule {
 					break
 				}
 			}
+			if !breaksRule {
+				new = append(new, page)
+				used[page] = true
+				break
+			}
 		}
 	}
-	return 0
+	return new[len(new)/2]
+}
+
+func deepCopy(a []int) []int {
+	new := []int{}
+	for _, v := range a {
+		new = append(new, v)
+	}
+	return new
 }
--- day5/main_test.go
+++ day5/main_test.go
@@ -17,3 +17,50 @@ func TestIsOk(t *testing.T) {
 		t.Fatalf("Expected true, got false")
 	}
 }
+
+func TestB(t *testing.T) {
+	pages := []int{75,97,47,61,53}
+	pageRules := map[int][]int{
+		75: []int{47, 61, 53, 29},
+		47: []int{61, 53, 29},
+		61: []int{53, 29},
+		53: []int{29},
+		97: []int{13,61,47,29,53,75},
+	}
+
+	if getMiddlePages(pages, pageRules) != 47 {
+		t.Fatalf("Expected 47, got %d", getMiddlePages(pages, pageRules))
+	}
+}
+
+func TestB2(t *testing.T) {
+	pages := []int{61,13,29}
+	pageRules := map[int][]int{
+		29: []int{13},
+		75: []int{47, 61, 53, 29},
+		47: []int{61, 53, 29},
+		61: []int{53, 29},
+		53: []int{29},
+		97: []int{13,61,47,29,53,75},
+	}
+
+	if getMiddlePages(pages, pageRules) != 29 {
+		t.Fatalf("Expected 29, got %d", getMiddlePages(pages, pageRules))
+	}
+}
+
+func TestB3(t *testing.T) {
+	pages := []int{97,13,75,29,47}
+	pageRules := map[int][]int{
+		29: []int{13},
+		75: []int{47, 61, 53, 29},
+		47: []int{61, 53, 29},
+		61: []int{53, 29},
+		53: []int{29},
+		97: []int{13,61,47,29,53,75},
+	}
+
+	if getMiddlePages(pages, pageRules) != 47 {
+		t.Fatalf("Expected 47, got %d", getMiddlePages(pages, pageRules))
+	}
+}
