b55a285a9905f693aa244dd0d6f60c522a24ab26
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-06 10:45:46 +0100

    day7 prep

---
+++ day7/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	file, err := os.Open(pwd + "/day7/atest.txt")
+	// file, err := os.Open(pwd + "/day7/btest.txt")
+	// file, err := os.Open(pwd + "/day7/input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	res := a(file)
+	// res := b(file)
+	fmt.Printf("%d\n", res)
+}
+
+func a(file io.Reader) int {
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, "")
+	}
+
+	return sum
+}
