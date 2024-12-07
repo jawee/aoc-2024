d822bbf3865dff1f1325f821702a16e5f39b55a7
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-07 14:53:31 +0100

    day8 prep

--- day7/main_test.go
+++ day7/main_test.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"testing"
-)
+) 
 
 func Test1(t *testing.T) {
 	numbers:= []int{81,40,27}
 
-	if !isValid(3267, numbers) {
+	if !isValidA(3267, numbers) {
 		t.Fatalf("Got false, expected true for %+v\n", numbers)
 	}
 }
@@ -17,7 +17,7 @@ func Test2(t *testing.T) {
 // 6: 2 3
 	// 2: 3 1 2
 // 6: 2 3 3
-	if isValid(2, numbers) {
+	if isValidA(2, numbers) {
 		t.Fatalf("Got true, expected false for %+v\n", numbers)
 	}
 }
