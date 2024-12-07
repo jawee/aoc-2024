08df62f67cbfcb4d414e94786b4ffd1e5517413d
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-07 10:57:50 +0100

    doesn't work

--- day7/main.go
+++ day7/main.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	pwd, _ := os.Getwd()
-	file, err := os.Open(pwd + "/day7/atest.txt")
+	// file, err := os.Open(pwd + "/day7/atest.txt")
 	// file, err := os.Open(pwd + "/day7/btest.txt")
-	// file, err := os.Open(pwd + "/day7/input.txt")
+	file, err := os.Open(pwd + "/day7/input.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -25,24 +25,100 @@ func main() {
 	fmt.Printf("%d\n", res)
 }
 
-func a(file io.Reader) int {
+func a(file io.Reader) uint64 {
 	scanner := bufio.NewScanner(file)
-	sum := 0
+	var sum uint64 = 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		split := strings.Split(line, ":")
 
-		wantedResult, err := strconv.Atoi(split[0])
+		wantedResult, err := strconv.ParseUint(split[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
 
 		numbersStr := strings.Split(strings.Trim(split[1], " "), " ")
 
-		numbers := []int{}
+		numbers := []uint64{}
 		for _, v := range numbersStr {
-
-			val, _ := strconv.Atoi(v)
+			val, err := strconv.ParseUint(v, 10, 64)
+			if err != nil {
+				panic(err)
+			}
 			numbers = append(numbers, val)
 		}
+
+		if isValid(wantedResult, numbers) {
+			sum += uint64(wantedResult)
+		}	
 	}
 
 	return sum
+}
+
+func createPossibleOperators(count int) [][]string {
+	possibleCombinations := [][]string{}
+	first := []string{}
+	last := []string{}
+	for _ = range count {
+		first = append(first, "+")
+		last = append(last, "*")
+	}
+	possibleCombinations = append(possibleCombinations, first)
+	for i := range count {
+		generated := deepCopy(first)
+		generated[i] = "*"
+		possibleCombinations = append(possibleCombinations, generated)
+		for j := range count {
+			if i == j {
+				continue
+			}
+			newGenerated := deepCopy(generated)
+			newGenerated[j] = "*"
+			possibleCombinations = append(possibleCombinations, newGenerated)
+		}
+	}
+	possibleCombinations = append(possibleCombinations, last)
+	return possibleCombinations
+}
+func deepCopy(a []string) []string {
+	new := []string{}
+	for _, v := range a {
+		new = append(new, v)
+	}
+	return new
+}
+
+func isValid(wantedResult uint64, numbers []uint64) bool {
+	// possibleCombinations := [][]string{
+	// 	{"+", "+"},
+	// 	{"*", "+"},
+	// 	{"*", "*"},
+	// 	{"+", "*"},
+	// }
+	possibleCombinations := createPossibleOperators(len(numbers)-1)
+
+	for _, operators := range possibleCombinations {
+		var sum uint64 = 1
+		fmt.Printf("Op: %+v\n", operators)
+		for i, v := range numbers {
+			fmt.Printf("%d: Handling %d\n", i, v)
+			if i == 0 {
+				sum *= uint64(v)
+				continue
+			}
+			if operators[i-1] == "+" {
+				sum += uint64(v)
+			}
+			if operators[i-1] == "*" {
+				sum *= uint64(v)
+			}
+		}
+
+		if sum == uint64(wantedResult) {
+			return true
+		}
+	}
+
+	return false
 }
--- day7/main_test.go
+++ day7/main_test.go
@@ -1,8 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test1(t *testing.T) {
+	numbers:= []uint64{81,40,27}
+
+	if !isValid(3267, numbers) {
+		t.Fatalf("Got false, expected true for %+v\n", numbers)
+	}
+}
+
+func Test2(t *testing.T) {
+	numbers := []uint64{3,1,2}
+// 6: 2 3
+	// 2: 3 1 2
+// 6: 2 3 3
+	if isValid(2, numbers) {
+		t.Fatalf("Got true, expected false for %+v\n", numbers)
+	}
+}
+
+func TestOperators(t *testing.T) {
+	res := createPossibleOperators(5)
+
+	for _, r := range res {
+		for _, v := range r {
+			fmt.Printf("%s ", v)
+		}
+
+		// t.Logf("\n")
+		fmt.Printf("\n")
+	}
+
+	t.FailNow()
 }
