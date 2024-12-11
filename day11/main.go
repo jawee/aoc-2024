7bd927aa4fb67d30ece90273c5ebfe33bf78ca63
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-11 15:27:02 +0100

    day11 a solved. as expected day b wont brute force

--- day11/main.go
+++ day11/main.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	pwd, _ := os.Getwd()
-	file, err := os.Open(pwd + "/day11/atest.txt")
+	// file, err := os.Open(pwd + "/day11/atest.txt")
 	// file, err := os.Open(pwd + "/day11/btest.txt")
-	// file, err := os.Open(pwd + "/day11/input.txt")
+	file, err := os.Open(pwd + "/day11/input.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -26,12 +27,52 @@ func main() {
 
 func a(file io.Reader) int {
 	scanner := bufio.NewScanner(file)
-	sum := 0
+	// sum := 0
 
+	input := []int{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		str := strings.Split(line, "")
-	}
+		split := strings.Split(line, " ")
 
-	return sum
+		for _, v := range split {
+			v, _ := strconv.Atoi(v)
+			input = append(input, v)
+		}
+
+	}
+	result := blink(input)
+	for range 74 {
+		result = blink(result)
+	}
+		
+
+	return len(result)
+}
+
+func blink(input []int) []int {
+	result := []int{}
+
+	for _, v := range input {
+		strV := fmt.Sprintf("%d", v)
+		if v == 0 {
+			result = append(result, 1)
+		} else if len(strV) % 2 == 0 {
+			//split
+			v1, err := strconv.Atoi(strV[:len(strV)/2])
+			if err != nil {
+				fmt.Printf("%s\n", err)
+			}
+			v2, err := strconv.Atoi(strV[len(strV)/2:])
+			if err != nil {
+				fmt.Printf("%s\n", err)
+			}
+			result = append(result, v1)
+			result = append(result, v2)
+
+		} else {
+			// multiply with 2048
+			result = append(result, v*2024)
+		}
+	}
+	return result
 }
--- day11/main_test.go
+++ day11/main_test.go
@@ -1,8 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test1(t *testing.T) {
+	input := []int{0, 1, 10, 99, 999,}
+	expected := []int{1, 2024, 1, 0, 9, 9, 2021976,}
+
+	res := blink(input)
+
+	if len(res) != len(expected) {
+		fmt.Printf("Res: %+v\n", res)
+		fmt.Printf("Exp: %+v\n", expected)
+		t.Fatalf("Failed on len")
+	}
+	for i := range expected {
+		if res[i] != expected[i] {
+			t.Fatalf("Failed on idx %d\n", i)
+		}
+	}
+}
+
+func Test2(t *testing.T) {
+	input := []int{125, 17}
+	expected := []int{2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2,}
+
+	res := blink(input)
+	for range 5 {
+		res = blink(res)
+	}
+
+	if len(res) != len(expected) {
+		fmt.Printf("Res: %+v\n", res)
+		fmt.Printf("Exp: %+v\n", expected)
+		t.Fatalf("Failed on len")
+	}
+	for i := range expected {
+		if res[i] != expected[i] {
+			t.Fatalf("Failed on idx %d\n", i)
+		}
+	}
 }
