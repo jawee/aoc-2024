6293e7e877fe18ce49eb319f17298749cab4a1c1
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-12 09:10:34 +0100

    day12 a

--- day12/main.go
+++ day12/main.go
@@ -10,9 +10,9 @@ import (
 
 func main() {
 	pwd, _ := os.Getwd()
-	file, err := os.Open(pwd + "/day12/atest.txt")
+	// file, err := os.Open(pwd + "/day12/atest.txt")
 	// file, err := os.Open(pwd + "/day12/btest.txt")
-	// file, err := os.Open(pwd + "/day12/input.txt")
+	file, err := os.Open(pwd + "/day12/input.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -28,9 +28,128 @@ func a(file io.Reader) int {
 	scanner := bufio.NewScanner(file)
 	sum := 0
 
+	grid := [][]string{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		split := strings.Split(line, " ")
+		split := strings.Split(line, "")
+		grid = append(grid, split)
 	}
+	sum = findFencingCost(grid)
 	return sum
+}
+
+type coordinate struct {
+	x int
+	y int
+}
+
+type area struct {
+	letter string
+	area []coordinate
+}
+
+func findFencingCost(grid [][]string) int {
+	areas := []area{}
+	visitedArea := [][]bool{}
+	for range grid {
+		visitedArea = append(visitedArea, make([]bool, len(grid[0])))
+	}
+
+	for row := range grid {
+		for col := range grid[row] {
+			if visitedArea[row][col] {
+				continue
+			}
+			letter := grid[row][col]
+			visited := []coordinate{}
+			queue := []coordinate{}
+			queue = append(queue, coordinate{x: col, y: row})
+
+			visited = floodFill(letter, queue, visited, grid)
+			
+			for _, v := range visited {
+				visitedArea[v.y][v.x] = true
+			}
+
+			areas = append(areas, area{letter: letter, area: visited})
+		}
+	}
+
+	sum := 0
+	for _, v := range areas {
+		// fmt.Printf("Area %s: %d\n", k, len(v))
+		fences := 0
+		for _, c := range v.area {
+			row := c.y
+			col := c.x
+
+			//up
+			if row-1 < 0 || grid[row-1][col] != v.letter {
+				fences += 1
+			}
+
+			//right
+			if col+1 >= len(grid[0]) || grid[row][col+1] != v.letter {
+				fences += 1
+			}
+
+			// down
+			if row+1 >= len(grid) || grid[row+1][col] != v.letter {
+				fences += 1
+			}
+
+			// left
+			if col-1 < 0 || grid[row][col-1] != v.letter {
+				fences += 1
+			}
+		}
+
+		// fmt.Printf("Fences for %s: %d, %d = %d\n", v.letter, len(v.area), fences, len(v.area)*fences)
+
+		sum += (len(v.area) * fences)
+	}
+
+	return sum
+}
+
+func floodFill(letter string, queue []coordinate, visited []coordinate, grid [][]string) []coordinate {
+
+	for len(queue) > 0 {
+		n := queue[0]
+		queue = queue[1:]
+
+		if contains(visited, n) {
+			continue
+		}
+
+		if n.x < 0 || n.x > len(grid[0])-1 || n.y < 0 || n.y > len(grid)-1 {
+			continue
+		}
+
+		if letter != grid[n.y][n.x] {
+			continue
+		}
+
+		visited = append(visited, n)
+
+		//top
+		queue = append(queue, coordinate{x: n.x, y: n.y + 1})
+		// bottom
+		queue = append(queue, coordinate{x: n.x, y: n.y - 1})
+		// right
+		queue = append(queue, coordinate{x: n.x + 1, y: n.y})
+		// left
+		queue = append(queue, coordinate{x: n.x - 1, y: n.y})
+	}
+
+	return visited
+}
+
+func contains(visited []coordinate, n coordinate) bool {
+	for _, v := range visited {
+		if v.x == n.x && v.y == n.y {
+			return true
+		}
+	}
+	return false
 }
--- day12/main_test.go
+++ day12/main_test.go
@@ -5,7 +5,44 @@ import (
 )
 
 func Test1(t *testing.T) {
+	input := [][]string{
+		{"A","A","A","A"},
+		{"B","B","C","D"},
+		{"B","B","C","C"},
+		{"E","E","E","C"},
+	}
+
+	res := findFencingCost(input)
+	if res != 140 {
+		t.Fatalf("Expected %d, got %d\n", 140, res)
+	}
 }
 
 func Test2(t *testing.T) {
+	input := [][]string{
+		{"O","O","O","O","O"},
+		{"O","X","O","X","O"},
+		{"O","O","O","O","O"},
+		{"O","X","O","X","O"},
+		{"O","O","O","O","O"},
+	}
+
+	res := findFencingCost(input)
+	if res != 772 {
+		t.Fatalf("Expected %d, got %d\n", 772, res)
+	}
+}
+
+func Test3(t *testing.T) {
+	input := [][]string{
+		{"A","A","A","A"},
+		{"B","B","C","D"},
+		{"B","B","C","C"},
+		{"E","E","E","C"},
+	}
+
+	res := floodFill("A", []coordinate{{x: 0, y: 0}}, []coordinate{}, input)
+	if len(res) != 4 {
+		t.Fatalf("Expected 4, got %d\n", len(res))
+	}
 }
